dac35a251099921c6de27ed3fc1f5ded645e5b1b
krishnamohan4225 <krishna.pvkm@gmail.com>
2018-12-19 21:33:25 +0530

    sending data from view to controller
    sending data from view to controller using request object and form collection
    use training1 controller and index.cshtml to run the page

---
+++ Training1Controller.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Training_2.Controllers
+{
+    public class Training1Controller : Controller
+    {
+        // GET: Training1
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        //code to send data from view to controller using form tag in view and request object in controller
+        [HttpPost]
+        public ActionResult postdata()
+        { 
+            string myname = Request["myname"];
+            string mycity = Request["mycity"];
+            ViewBag.myname = myname;
+            ViewBag.mycity = mycity;
+            return View("Index");
+        }
+
+        [HttpPost]
+        public ActionResult postdata_formcollection(FormCollection f)
+        {
+            string myname = f["myname"];
+            string mycity = f["mycity"];
+            ViewBag.myname = myname;
+            ViewBag.mycity = mycity;
+            return View("Index");
+        }
+
+
+    }
+}
